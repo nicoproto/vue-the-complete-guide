726736c74eb9a86493923e1c0dc9e9725a5181da
Nico Proto <nicolasproto@gmail.com>
2021-01-29 11:49:04 +0100

    Continued course

---
+++ 02-course_apps/03-cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,36 @@
+<template>
+  <form @submit.prevent="updateUser">
+    <div>
+      <input type="text" v-model="name" placeholder="Your name">
+    </div>
+    <div>
+      <input type="text" v-model="age" placeholder="Your age">
+    </div>
+    <button type="submit">Update info</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: {
+    'update-user': (name, age) => {
+      if (name === '' || age === '') {
+        return false;
+      } else {
+        return true;
+      }
+    }
+    },
+  data() {
+    return {
+      name: '',
+      age: ''
+    }
+  },
+  methods: {
+    updateUser() {
+      this.$emit('update-user', this.name, this.age);
+    }
+  }
+}
+</script>
