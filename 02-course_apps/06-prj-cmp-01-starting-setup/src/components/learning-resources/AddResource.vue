beae40f9177e92405e4c81ca7dc947bf4cec5835
Nico Proto <nicolasproto@gmail.com>
2021-01-30 19:24:18 +0100

    Continued course

---
+++ 02-course_apps/06-prj-cmp-01-starting-setup/src/components/learning-resources/AddResource.vue
@@ -0,0 +1,90 @@
+<template>
+  <teleport to="body">
+    <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+
+      <template #default>
+        <p>Unfortunately at least one input value is invalid.</p>
+        <p>Please check all inputs and make sure you enter a few characters into each input field.</p>
+      </template>
+      <template #actions>
+        <base-button @click="confirmError">Okay</base-button>
+      </template>
+    </base-dialog>
+  </teleport>
+  <base-card>
+    <form @submit.prevent="submitData" ref="resourceForm">
+      <div class="form-control">
+        <label for="title">Title</label>
+        <input id="title" name="title" type="text" ref="titleInput" />
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea id="description" name="description" rows="3" ref="descriptionInput" />
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input id="link" name="link" type="text" ref="linkInput" />
+      </div>
+      <div>
+        <base-button type="submit">Add Resource</base-button>
+      </div>
+    </form>
+  </base-card>
+</template>
+
+<script>
+export default {
+  inject: ['addResource'],
+  data() {
+    return {
+      inputIsInvalid: false,
+    }
+  },
+  methods: {
+    submitData() {
+      const enteredTitle = this.$refs.titleInput.value;
+      const enteredDescription = this.$refs.descriptionInput.value;
+      const enteredLink = this.$refs.linkInput.value;
+
+      if (enteredTitle.trim() === '' || enteredDescription.trim() === '' || enteredLink.trim() === '') {
+        this.inputIsInvalid = true;
+        return;
+      }
+      this.inputIsInvalid = false;
+      this.addResource(enteredTitle, enteredDescription, enteredLink);
+      this.$refs.resourceForm.reset()
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    }
+  }
+};
+</script>
+
+<style scoped>
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+</style>
