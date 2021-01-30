f0a435757b713b1d6ad36dc17e513781a6f91eda
Nico Proto <nicolasproto@gmail.com>
2021-01-30 19:24:12 +0100

    Continued course

---
+++ 02-course_apps/05-cmp-adv-01-starting-setup/src/components/UserInfo.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <base-card>
+      <template #header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <template #default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import BaseBadge from './UI/BaseBadge.vue';
+import BaseCard from './UI/BaseCard.vue';
+
+export default {
+  components: {
+    BaseBadge,
+    BaseCard,
+  },
+  props: ['fullName', 'infoText', 'role'],
+};
+</script>
