d572eddee659ba4f8916ebb3558ab8cc4a8393e7
Nico Proto <nicolasproto@gmail.com>
2021-02-02 09:46:37 +0100

    Continued course

---
+++ 02-course_apps/09-routing-01-starting-setup/src/pages/UsersList.vue
@@ -0,0 +1,66 @@
+<template>
+<button @click="confirmInput">Confirm</button>
+<button @click="saveChanges">Save changes</button>
+  <ul>
+    <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
+  </ul>
+</template>
+
+<script>
+import UserItem from '../components/users/UserItem.vue';
+
+export default {
+  components: {
+    UserItem,
+  },
+  inject: ['users'],
+  data() {
+    return {
+      changesSaved: false,
+    }
+  },
+  methods: {
+    confirmInput() {
+      // do something
+      this.$router.push('/teams');
+    },
+    saveChanges() {
+      this.changesSaved = true;
+    }
+  },
+  beforeRouteEnter(to, from, next) {
+    // console.log('UsersList Cmp beforeRouteEnter');
+    // console.log(to, from);
+    next();
+  },
+  beforeRouteLeave(to, from, next) {
+    if (this.changesSaved) {
+      next();
+    } else {
+      const userWantsToLeave = confirm('Are you sure? You have unsaved changes!');
+      next(userWantsToLeave);
+    }
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  max-width: 20rem;
+  padding: 0;
+}
+
+button {
+  font: inherit;
+  background: blue;
+  border: 1px solid transparent;
+  cursor: pointer;
+  color: white;
+  padding: 0.5rem 1.5rem;
+  margin: 1rem;
+  display: inline-block;
+  text-decoration: none;
+}
+</style>
