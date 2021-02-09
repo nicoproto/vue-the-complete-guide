8570a1c5609da37cea7a139a2d8ef73f66078283
Nico Proto <nicolasproto@gmail.com>
2021-02-09 10:28:02 +0100

    Continued course

---
+++ 02-course_apps/20-composition-19-vuex-starting-project/src/components/ControlCenter.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="inc">Increment</button>
+</template>
+
+<script>
+import { useStore } from "vuex";
+
+export default {
+  setup() {
+    const store = useStore();
+
+    const inc = () => {
+      store.dispatch('increment');
+    }
+
+    return { inc };
+  },
+};
+</script>
