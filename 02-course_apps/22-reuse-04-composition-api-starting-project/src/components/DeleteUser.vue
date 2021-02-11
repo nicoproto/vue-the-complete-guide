8ebcd161ad99caff64090a3451eb51660043ae31
Nico Proto <nicolasproto@gmail.com>
2021-02-11 16:27:46 +0100

    Continued course

---
+++ 02-course_apps/22-reuse-04-composition-api-starting-project/src/components/DeleteUser.vue
@@ -0,0 +1,33 @@
+<template>
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
+    <p>Do you want to continue with deleting a user?</p>
+  </user-alert>
+  <section>
+    <h2>Delete a User</h2>
+    <button @click="showAlert">Delete User</button>
+  </section>
+</template>
+
+<script>
+import { ref } from 'vue';
+import UserAlert from './UserAlert.vue';
+import useAlert from '../hooks/alert.js'
+
+export default {
+  components: {
+    UserAlert,
+  },
+  setup() {
+    const alertTitle = ref('Delete banana?');
+
+    const { alertIsVisible, hideAlert, showAlert } = useAlert();
+
+    return {
+      alertIsVisible,
+      showAlert,
+      hideAlert,
+      alertTitle
+    };
+  }
+};
+</script>
